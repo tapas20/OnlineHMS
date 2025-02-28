30810cbeca11be7d344b939c3e43cf62f057993d
Tapas Jyoti <tapasjyoti8327@gmail.com>
2025-02-28 10:27:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='font-bold text-2xl bg-gray-200 p-4 text-red-500'>
+      <p>Hello World....</p>
+    </div>
+  )
+}
+
+export default App
