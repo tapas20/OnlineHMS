10152d2b67271c9801ec0cc535b556cb87dbc74a
krishnakumarmahakul <krishnakumahakul@gmail.com>
2025-03-03 21:24:57 +0530

    Add landing page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
-
+import styles from './About.module.css'
 function About() {
   return (
-    <div>About</div>
+    <>
+      <div className={styles.aboutParent}>
+          <div className={styles.top}>
+            <h3>DocPulse Quantitative Over View</h3>
+            <span> Founder in 2025 with a foot print spaning across india ,the Middle East and Africa</span>
+            
+            
+          </div>
+          <div className={styles.secondTop}>
+          <h3>3,000+</h3>
+            <p>Doctors are growing</p>
+            <h3>300+</h3>
+            <p>clients</p>
+          </div>
+          <div className={styles.boxContainer}>
+            <div className={styles.box}>
+              <h4>Our Vison</h4>
+              <p>Empower healthcare enterprises to deliver
+                 exceptional healthcare
+                 services and enhance patient care worldwide
+                  through the utilization of advanced technology.</p>
+            </div>
+            <div className={styles.box}>
+              <h4>Our Mission</h4>
+              <p>Provide an affordable and secure healthcare 
+                platform for doctors, clinics, 
+                and hospitals, delivering exceptional value at 
+                a competitive price for optimized healthcare solutions.</p>
+            </div>
+          </div>
+          <div className={styles.valueContent}>
+            <h3>
+              Value
+            </h3>
+            <p>Our core values shape our identity and serve as a 
+              stabilizing force during challenging times.
+               Our organizational culture prioritizes customer focus,
+                ongoing enhancement, and dedicated customer service.</p>
+          </div>
+      </div>
+    </>
   )
 }
 
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import styles from './Landing.module.css'
+import landingImg from '../../assets/hms1.jpg'
+import About from '../About/About'
 function Landing() {
   return (
-    <div>Landing</div>
+    <>
+    <div className={styles.landing}>
+      <div className={styles.landingimg}>
+        <img src={landingImg} alt="Healthcare Solution" />
+        <div className={styles.overlayText}>
+          <h1 className={styles.main}>Explore Comprehensive Cloud-Based Healthcare Software Solutions Tailored for Hospitals, Clinics, and Doctors</h1>
+          <p className={styles.mainp}>Experience state-of-the-art digital solutions with DocPulse’s
+            and HIMS(Hospital Integrated Management Software),
+            CMS(Clinic Management Software), PMS(Patient Management Software),
+            designed to meet the unique needs of doctors, clinics, and hospitals.</p>
+        </div>
+      </div>
+        <div className={styles.landingText}>
+          <span>
+            Your <span className={styles.highlight1}>health</span> is our <span className={styles.highlight2}>priority</span>.
+            We are committed to providing <span className={styles.highlight3}>high-quality</span> medical care
+            with <span className={styles.highlight1}>advanced treatments</span>,
+            <span className={styles.highlight2}>experienced doctors</span>, and a <span className={styles.highlight3}>patient-first approach</span>.
+            From <span className={styles.highlight1}>routine check-ups</span> to
+            <span className={styles.highlight2}>specialized care</span>, we ensure
+            a <span className={styles.highlight3}>safe and healing environment</span> for you and your loved ones.
+          </span>
+        </div>
+    </div>
+
+    <h1>this is the aout page </h1>
+    <About/>
+    </>
   )
 }
 
