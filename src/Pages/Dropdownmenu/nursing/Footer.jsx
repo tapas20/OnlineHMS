937e7b3dbc1353e05e24ed3e171cf9a3afa79c02
Saubhagya-2004 <saubhagyabaliarsingh2@gmail.com>
2025-07-02 22:02:02 +0530

    Nursing service

--- src/Pages/Dropdownmenu/nursing/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-  <footer className="bg-gray-800 text-white p-4 text-center">
-    &copy; 2025 Nursing Means. All rights reserved.
-  </footer>
-);
-
-export default Footer;
